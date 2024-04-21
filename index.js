5b46af661645f7a092bfc404c63a1c2e675ef6cf
AriyanMlzm <ryanmolazem@gmail.com>
2024-04-21 20:45:17 +0330

    set dall-e routes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 
+import router from './routes/dalle.routes.js'
+
 dotenv.config()
 
 const port = 8080
@@ -9,6 +11,8 @@ const port = 8080
 const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mg' }))
+
+app.use('/api/v1/dalle', router)
 
 app.get('/', (req, res) => {
   res.status(200).json({ msg: 'Hello?' })
