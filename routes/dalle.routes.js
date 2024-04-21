a5cf1780a83857a0c9cb600c3e085506553211c8
AriyanMlzm <ryanmolazem@gmail.com>
2024-04-21 22:33:51 +0330

    setup openai

--- routes/dalle.routes.js
+++ routes/dalle.routes.js
@@ -6,6 +6,10 @@ dotenv.config()
 
 const router = express.Router()
 
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_KEY
+})
+
 router.route('/').get((req, res) => {
   res.status(200).json({ msg: 'Hello DALL.E?' })
 })
